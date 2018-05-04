8b5534548db5cd027946c25f9b5d6a6f7dac4e69
Olivier <Olivier@LOK111-03>
2018-05-04 12:40:07 +0200

    een falende test

--- src/test/java/be/vdab/goededoel/entities/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/entities/GoedeDoelTest.java
@@ -2,21 +2,27 @@ package be.vdab.goededoel.entities;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigDecimal;
+
 import org.junit.Before;
 import org.junit.Test;
 
-import be.vdab.goededoel.entities.GoedeDoel;
-
 public class GoedeDoelTest {
-	private static final String NAAM ="CliniClowns"; 
+	private static final String NAAM = "CliniClowns";
 	private GoedeDoel doel;
 
 	@Before
 	public void before() {
 		doel = new GoedeDoel(NAAM);
 	}
+
 	@Test
 	public void getNaam() {
 		assertEquals(NAAM, doel.getNaam());
+	}
+
+	@Test
+	public void eenNieuwDoelHeeftNogNietsOpgebracht() {
+		assertEquals(0, doel.getOpgebracht().compareTo(BigDecimal.ZERO));
 	}
 }
