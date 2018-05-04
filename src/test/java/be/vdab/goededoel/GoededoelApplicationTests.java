ad60d3fd85a7dc181f045f3f1fad289a20693fe7
Olivier <Olivier@LOK111-03>
2018-05-04 10:43:57 +0200

    eerste commit

---
+++ src/test/java/be/vdab/goededoel/GoededoelApplicationTests.java
@@ -0,0 +1,16 @@
+package be.vdab.goededoel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoededoelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
