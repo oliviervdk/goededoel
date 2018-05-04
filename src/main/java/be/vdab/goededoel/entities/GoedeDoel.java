50aad322b5c444371353929c2bc400ee8039b22c
Olivier <Olivier@LOK111-03>
2018-05-04 12:47:25 +0200

    +equals in GoedeDoel.java

--- src/main/java/be/vdab/goededoel/entities/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/entities/GoedeDoel.java
@@ -5,13 +5,25 @@ import java.math.BigDecimal;
 public class GoedeDoel {
 	private final String naam;
 	private BigDecimal opgebracht = BigDecimal.ZERO;
+
 	public GoedeDoel(String naam) {
 		this.naam = naam;
 	}
+
 	public String getNaam() {
 		return naam;
 	}
+
 	public BigDecimal getOpgebracht() {
 		return opgebracht;
 	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (!(object instanceof GoedeDoel)) {
+			return false;
+		}
+		return ((GoedeDoel) object).naam.equalsIgnoreCase(this.naam);
+	}
+
 }
